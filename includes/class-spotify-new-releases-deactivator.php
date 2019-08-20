d61c7e08431cd903fc20488714f3ac3e892f1777
MK <michal10203040@gmail.com>
2019-08-20 15:40:48 +0200

    first commit

---
+++ includes/class-spotify-new-releases-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://chickencross.se
+ * @since      1.0.0
+ *
+ * @package    Spotify_New_Releases
+ * @subpackage Spotify_New_Releases/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Spotify_New_Releases
+ * @subpackage Spotify_New_Releases/includes
+ * @author     Michal Kurowski <michal.kurowski98@outlook.com>
+ */
+class Spotify_New_Releases_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
